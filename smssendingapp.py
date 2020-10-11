3120eb21cc437c6820e27f12d9e2de46d5c7ac5e
PrachiShah01 <pr01hi.shah@gmail.com>
2020-10-11 23:28:34 +0530

    final commit

--- smssendingapp.py
+++ smssendingapp.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
-auth_token = 'your_auth_token'
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' #Enter your Account SID here 
+auth_token = 'your_auth_token' #Enter Token here
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-    from_='+15017122661',
-    body ='body',
-    to ='+15558675310'
+    from_='+15017122661',  #Enter trial number
+    body ='body', #Enter message here
+    to ='+15558675310' #Enter verifred number with your twilio account
 )
 print(message.sid)
